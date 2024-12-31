f25fddbdfdcaa7534f38b292f66f8aceb585ceaa
Radioserk <boujahdachraf@gmail.com>
2024-12-31 17:26:57 +0100

    connexion data base config

--- Connectdb.php
+++ Connectdb.php
@@ -1,29 +1,24 @@
 <?php
-
-class Database
-{
+class Database {
     private $host = "localhost";
-    private $dbName = "Driveloc";
     private $username = "root";
     private $password = "";
-    private $Connectpdo;
+    private $dbname = "drive";
+    public $connectpdo;
 
-    public function __construct()
-    {
-        // Try to connect to the database
+    public function __construct() {
         try {
-            $dsn = "mysql:host={$this->host};dbname={$this->dbName}";
-            $this->pdo = new PDO($dsn, $this->username, $this->password);
-
-            // Best practice because of it help catching errrors
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connectpdo = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+            $this->connectpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            die("Database connection failed: " . $e->getMessage());
+            echo "Connection échouée: " . $e->getMessage();
         }
     }
-
-    public function prepare($sql)
-    {
-        return $this->pdo->prepare($sql);
+    public function query($sql) {
+        return $this->connectpdo->query($sql);
+    }
+    public function prepare($sql) {
+        return $this->connectpdo->prepare($sql);
     }
 }
+?>
